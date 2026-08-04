de68dba59a9079ef160e0b553dcb72a1b471b0bf
AhmedAlSunbati712 <ahmed.m.al.sunbati.27@dartmouth.edu>
2026-08-03 23:27:39 -0700

    chore: Log setup and new segment helper

--- internal/server/log/log.go
+++ internal/server/log/log.go
@@ -2,6 +2,9 @@ package log
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -28,5 +31,35 @@ func newLog(dir string, c Config) (*Log, error) {
 }
 
 func (l *Log) setup() error {
+	files, err := os.ReadDir((l.Dir))
+	if err != nil {
+		return err
+	}
+	offsets := make([]uint64, 10)
+	for _, file := range files {
+		parts := strings.Split(file.Name(), ".")
+		file_name, file_extension := parts[0], parts[1]
+		if file_extension == ".store" {
+			file_base_offset, err := strconv.ParseUint(file_name, 10, 0)
+			if err != nil {
+				return err
+			}
+			offsets = append(offsets, file_base_offset)
+		}
+	}
+
+	for _, offset := range offsets {
+		l.newSegment(offset)
+	}
 	return fmt.Errorf("Some placeholder")
+}
+
+func (l *Log) newSegment(offset uint64) error {
+	segment, err := newSegment(l.Dir, offset, l.c)
+	if err != nil {
+		return err
+	}
+	l.activeSegment = segment
+	l.segments = append(l.segments, segment)
+	return nil
 }
